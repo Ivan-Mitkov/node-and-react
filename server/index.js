c4fe7992e1502476e55d052bb53974108c10cf1d
unknown <ivan.mitkov@gmail.com>
2019-07-28 09:42:07 +0300

    heroku settings

--- server/index.js
+++ server/index.js
@@ -5,5 +5,6 @@ app.get('/',(req,res)=>{
     res.send({hi:'there'})
 })
 
-app.listen(5000)
-console.log('app listen to 5000')
+const PORT =process.env.PORT||5000;
+app.listen(PORT)
+console.log(`app listen to${PORT}`)
